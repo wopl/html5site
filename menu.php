6ebdc7bb6ea1251814a858396122fa985338554c
wopl <git61@safe-mail.net>
2014-09-12 05:01:21 +0200

    140912 base menu working

--- menu.php
+++ menu.php
@@ -5,23 +5,48 @@
 <!-- ---------------------------------------------------------------------------- -->
 
 <ul>
-   <li class='active'><a href='home.html'><span>Home</span></a></li>
-   <li class='active has-sub'><a href='#'><span>Products</span></a>
+   <?php
+		if ($section == "home"){
+			   echo "<li class='active'>";
+		} else {
+			   echo "<li>";
+		}
+		echo "<a href='index.php?section=home'><span>Home</span></a></li>";
+   ?>
+
+   <li class='has-sub'><a href='#'><span>Products</span></a>
       <ul>
-         <li class='active has-sub'><a href='#'><span>Product 1</span></a>
+         <li class='has-sub'><a href='#'><span>Product 1</span></a>
             <ul>
-               <li><a href='#'><span>Sub Product</span></a></li>
-               <li class='last'><a href='#'><span>Sub Product</span></a></li>
+               <li><a href='#'><span>Sub Product11</span></a></li>
+               <li class='active last'><a href='#'><span>Sub Product12</span></a></li>
             </ul>
          </li>
          <li class='has-sub'><a href='#'><span>Product 2</span></a>
             <ul>
-               <li><a href='#'><span>Sub Product</span></a></li>
-               <li class='last'><a href='#'><span>Sub Product</span></a></li>
+               <li><a href='#'><span>Sub Product21</span></a></li>
+               <li class='last'><a href='#'><span>Sub Product22</span></a></li>
             </ul>
          </li>
       </ul>
    </li>
-   <li><a href='#'><span>About</span></a></li>
-   <li class='last'><a href='#'><span>Contact</span></a></li>
+<!--   
+   <li><a href='index.php?section=about'><span>About</span></a></li>
+   <li class='last'><a href='index.php?section=contact'><span>Contact</span></a></li>
+-->
+   <?php
+		if ($section == "about"){
+			   echo "<li class='active'>";
+		} else {
+			   echo "<li>";
+		}
+		echo "<a href='index.php?section=about'><span>About</span></a></li>";
+
+		if ($section == "contact"){
+			   echo "<li class='active last'>";
+		} else {
+			   echo "<li>";
+		}
+		echo "<a href='index.php?section=contact'><span>Contact</span></a></li>";
+   ?>
 </ul>
