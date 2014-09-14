33a90ce356e43b9f511f4ea59c7366a8e83d86a6
wopl <git61@safe-mail.net>
2014-09-14 20:03:50 +0200

    first database access

--- database.php
+++ database.php
@@ -5,3 +5,29 @@
 <!-- ---------------------------------------------------------------------------- -->
 
 database
+<br />
+
+<?php
+$username="db11197344-root";
+$password="linux4me";
+$database="db11197344-protrack";
+$host="localhost";
+$conn=mysql_connect('localhost','db11197344-root','linux4me');
+mysql_select_db($database);
+
+$select="SELECT clicks FROM counter WHERE id='home'";
+$result=mysql_query($select);
+$row=mysql_fetch_array($result);
+$update="UPDATE counter SET clicks = clicks+1 WHERE id='home'";
+$change=mysql_query($update);
+?>
+
+<?php
+echo "Die Seite wurde ";
+echo $row["clicks"];
+echo " Mal angesehen";
+?>
+
+<?php
+mysql_close($conn);
+?>
